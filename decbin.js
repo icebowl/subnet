e8ef5a19a1d24e29cb5dff3a91533590732f58f5
icebowl <icebowl1967@gmail.com>
2021-01-23 20:57:50 -0800

    AA

---
+++ decbin.js
@@ -0,0 +1,15 @@
+const x = 2
+const y = 'y'
+console.log(x, y)
+console.log('x  %d %s', x, 'x y')
+
+/*
+
+    %s format a variable as a string
+    %d format a variable as a number
+    %i format a variable as its integer part only
+    %o format a variable as an object
+
+
+
+*/
